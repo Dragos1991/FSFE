3dabdb2b3ff0c93ccbd039e69ebd48f18cd88bfd
Dragos Nitescu <dragos@Dragoss-MacBook-Pro.local>
2021-12-09 12:58:20 +0200

    fist commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.send('Salutare! Aceasta este aplicatia lui Dragos');
 });
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`));
